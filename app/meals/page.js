87cfcb52bad4403904b5b23721f0d2820ddfe8e0
hojun.yoon <tjdsksro90@gmail.com>
2024-08-29 17:55:49 +0900

    ✨ update: 섹션 3: NextJS 핵심(앱 라우터) - Suspense & Streamed Response를 이용한 세분화 로딩 상태 관리

--- app/meals/page.js
+++ app/meals/page.js
@@ -2,9 +2,15 @@ import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
 import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
 
-const MealsPage = async () => {
+const Meals = async () => {
   const meals = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+};
+
+const MealsPage = () => {
   return (
     <>
       <header className={classes.header}>
@@ -20,7 +26,11 @@ const MealsPage = async () => {
         </p>
       </header>
       <main className={classes.main}>
-        <MealsGrid meals={meals} />
+        <Suspense
+          fallback={<p className={classes.loading}>Fetching meals...</p>}
+        >
+          <Meals />
+        </Suspense>
       </main>
     </>
   );
