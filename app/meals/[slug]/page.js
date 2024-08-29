f740e2a5c278713ff2e9ff2ccf81da21800e4a70
hojun.yoon <tjdsksro90@gmail.com>
2024-08-29 19:03:50 +0900

    ✨ update: 섹션 3: NextJS 핵심(앱 라우터) - 동적 경로와 경로 매개변수를 활용한 Meals 세부내용 로딩 및 렌더링

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,7 +1,33 @@
-import React from "react";
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
 
-const MealDetailsPage = () => {
-  return <h1>Meal Details</h1>;
+const MealDetailsPage = ({ params }) => {
+  const meal = getMeal(params.slug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
 export default MealDetailsPage;
