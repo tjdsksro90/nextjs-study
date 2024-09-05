1e648e5f97bca4c260bff5d070444c40949f9c31
hojun.yoon <tjdsksro90@gmail.com>
2024-09-05 18:56:24 +0900

    ✨ update: 섹션 6: 데이터 변이(심층분석) - useFormState에 대한 서버 액션 조정

--- app/new-post/page.js
+++ app/new-post/page.js
@@ -1,18 +1,15 @@
-"use client";
-
-import FormSubmit from "@/components/form-submit";
+import PostForm from "@/components/post-form";
 import { storePost } from "@/lib/posts";
 import { redirect } from "next/navigation";
-import { useActionState } from "react";
 
 export default function NewPostPage() {
-  async function createPost(formData) {
+  async function createPost(prevState, formData) {
     "use server";
     const title = formData.get("title");
     const image = formData.get("image");
     const content = formData.get("content");
 
-    let error = [];
+    let errors = [];
 
     if (!title || title.trim().length === 0) {
       console.log("Title is required");
@@ -20,12 +17,12 @@ export default function NewPostPage() {
     if (!content || content.trim().length === 0) {
       console.log("Content is required");
     }
-    if (!image) {
+    if (!image || image.size === 0) {
       console.log("Image is required");
     }
 
-    if (error.length > 0) {
-      return { error };
+    if (errors.length > 0) {
+      return { errors };
     }
 
     storePost({
@@ -38,33 +35,5 @@ export default function NewPostPage() {
     redirect("/feed");
   }
 
-  const [state, formAction] = useActionState(createPost, {});
-
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form action={formAction}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />;
 }
