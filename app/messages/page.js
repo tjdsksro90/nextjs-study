4017056d80c99c9b2543028d12c5e9564f12cdf7
hojun.yoon <tjdsksro90@gmail.com>
2024-10-10 16:47:04 +0900

    ✨ update: 섹션 7: 캐싱 이해 및 구성하기 - 데이터 캐싱 제어

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,12 @@
+import { unstable_noStore } from "next/cache"; // dynamic = "force-dynamic" 함수보다 권장하는 방법
 import Messages from "@/components/messages";
 
+// export const revalidate = 5; // NextJS에서 정한 해당 초만큼 재요청
+// export const dynamic = "force-dynamic"; // NextJS에서 정한 cache: 'no-store'과 같은 기능(항상 다시 가져옴)
+
 export default async function MessagesPage() {
-  const response = await fetch("http://localhost:8080/messages", {
-    next: {
-      revalidate: 5,
-    },
-  });
+  // unstable_noStore(); // 함수내에서 명시적으로 사용하기에 권장
+  const response = await fetch("http://localhost:8080/messages");
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
