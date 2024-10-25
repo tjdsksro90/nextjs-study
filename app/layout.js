e15e23b040b3c8baeca248dbbaeee6dd9b78b20e
hojun.yoon <tjdsksro90@gmail.com>
2024-10-26 00:44:32 +0900

    ✨ update: 섹션 8: NextJS 앱 최적화 - 레이아웃 메타데이터 이해

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/header';
 import './globals.css';
 
+// page.js 에서 설정해두지 않았다면 모든 페이지가 아래 metadata 적용(기본 느낌)
 export const metadata = {
   title: 'NextPosts',
   description: 'Browse and share amazing posts.',
--- app/page.js
+++ app/page.js
@@ -3,6 +3,23 @@ import { Suspense } from 'react';
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// 정적 메타데이터
+export const metadata = {
+  title: '...',
+  description: '...',
+};
+
+// 동적 메타데이터
+export async function generateMetadata(data) {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+
+  return {
+    title: `... ${numberOfPosts} ...`,
+    description: '...',
+  };
+}
+
 async function LatestPosts() {
   const latestPosts = await getPosts(2);
   return <Posts posts={latestPosts} />;
@@ -14,9 +31,9 @@ export default async function Home() {
       <h1>Welcome back!</h1>
       <p>Here's what you might've missed.</p>
       <section id="latest-posts">
-      <Suspense fallback={<p>Loading recent posts...</p>}>
-        <LatestPosts />
-      </Suspense>
+        <Suspense fallback={<p>Loading recent posts...</p>}>
+          <LatestPosts />
+        </Suspense>
       </section>
     </>
   );
