8017d3e9da10dab33dd4ce449cb5bac634271dcf
hojun.yoon <tjdsksro90@gmail.com>
2024-10-25 18:42:27 +0900

    ✨ update: 섹션 7: 캐싱 이해 및 구성하기 - 커스텀 데이터 소스 데이터 무효화

--- app/messages/new/page.js
+++ app/messages/new/page.js
@@ -1,18 +1,18 @@
-import { redirect } from 'next/navigation';
+import { redirect } from "next/navigation";
 
-import { addMessage } from '@/lib/messages';
+import { addMessage } from "@/lib/messages";
 import { revalidatePath, revalidateTag } from "next/cache";
 
 export default function NewMessagePage() {
   async function createMessage(formData) {
-    'use server';
+    "use server";
 
-    const message = formData.get('message');
+    const message = formData.get("message");
     addMessage(message);
-    redirect('/messages');
+    redirect("/messages");
     // revalidatePath('/');
-    // revalidatePath('/message');
-    revalidateTag("msg");
+    // revalidatePath('/message'); // 새 글 작성시 업데이트 안되는 문제 해결 방법 1
+    revalidateTag("msg"); // 새 글 작성시 업데이트 안되는 문제 해결 방법 2
   }
 
   return (
