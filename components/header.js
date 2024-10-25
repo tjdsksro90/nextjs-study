145f5a81005ea498dd9fd6f8f13ff18723c77594
hojun.yoon <tjdsksro90@gmail.com>
2024-10-25 20:44:26 +0900

    ✨ update: 섹션 8: NextJS 앱 최적화 - 우선순위 이미지 및 기타 설정으로 작업하기

--- components/header.js
+++ components/header.js
@@ -1,11 +1,13 @@
 import logo from '@/assets/logo.png';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+        {/* header에 있는 이미지 같은 경우 지연되면 안되기에 priority 추가 */}
+        <Image src={logo} width={100} height={100} priority alt="Mobile phone with posts feed on it" />
       </Link>
       <nav>
         <ul>
@@ -13,7 +15,9 @@ export default function Header() {
             <Link href="/feed">Feed</Link>
           </li>
           <li>
-            <Link className='cta-link' href="/new-post">New Post</Link>
+            <Link className="cta-link" href="/new-post">
+              New Post
+            </Link>
           </li>
         </ul>
       </nav>
