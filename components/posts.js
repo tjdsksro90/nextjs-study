a4c675d3a620065369d04b318dceaee5da838de9
hojun.yoon <tjdsksro90@gmail.com>
2024-10-26 00:32:19 +0900

    ✨ update: 섹션 8: NextJS 앱 최적화 - 이미지 로더 및 클라우드 크기 조정 사용

--- components/posts.js
+++ components/posts.js
@@ -1,33 +1,36 @@
-"use client";
+'use client';
 
 import { useOptimistic } from 'react';
 
 import { formatDate } from '@/lib/format';
 import LikeButton from './like-icon';
 import { togglePostLikeStatus } from '@/actions/posts';
+import Image from 'next/image';
+
+function imageLoader(config) {
+  const urlStart = config.src.split('upload')[0];
+  const urlEnd = config.src.split('upload')[1];
+  const transformations = `w_200,q_${config.quality}`;
+
+  return `${urlStart}upload/${transformations}/${urlEnd}`;
+}
 
 function Post({ post, action }) {
   return (
     <article className="post">
       <div className="post-image">
-        <img src={post.image} alt={post.title} />
+        <Image loader={imageLoader} src={post.image} fill alt={post.title} />
       </div>
       <div className="post-content">
         <header>
           <div>
             <h2>{post.title}</h2>
             <p>
-              Shared by {post.userFirstName} on{' '}
-              <time dateTime={post.createdAt}>
-                {formatDate(post.createdAt)}
-              </time>
+              Shared by {post.userFirstName} on <time dateTime={post.createdAt}>{formatDate(post.createdAt)}</time>
             </p>
           </div>
           <div>
-            <form
-              action={action.bind(null, post.id)}
-              className={post.isLiked ? 'liked' : ''}
-            >
+            <form action={action.bind(null, post.id)} className={post.isLiked ? 'liked' : ''}>
               <LikeButton />
             </form>
           </div>
@@ -40,7 +43,7 @@ function Post({ post, action }) {
 
 export default function Posts({ posts }) {
   const [optimisticPosts, updateOptimisticPosts] = useOptimistic(posts, (prevPosts, updatedPostId) => {
-    const updatedPostIndex = prevPosts.findIndex(post => post.id === updatedPostId);
+    const updatedPostIndex = prevPosts.findIndex((post) => post.id === updatedPostId);
 
     if (updatedPostIndex === -1) {
       return prevPosts;
@@ -52,7 +55,7 @@ export default function Posts({ posts }) {
     const newPosts = [...prevPosts];
     newPosts[updatedPostIndex] = updatedPost;
     return newPosts;
-  })
+  });
 
   if (!optimisticPosts || optimisticPosts.length === 0) {
     return <p>There are no posts yet. Maybe start sharing some?</p>;
